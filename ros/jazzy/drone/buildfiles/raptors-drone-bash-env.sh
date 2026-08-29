461715597195ea3ce2f31b57e4df90dafc2bdc17
Marcin Walczyk <marcinwal9@gmail.com>
2026-08-29 02:36:51 +0200

    more stuff in bash-env

--- ros/jazzy/drone/buildfiles/raptors-drone-bash-env.sh
+++ ros/jazzy/drone/buildfiles/raptors-drone-bash-env.sh
@@ -6,8 +6,10 @@ export GZ_SIM_RESOURCE_PATH=${GZ_SIM_RESOURCE_PATH:-/opt/ardupilot_gazebo/models
 export DISPLAY=${DISPLAY:-:0}
 export QT_X11_NO_MITSHM=${QT_X11_NO_MITSHM:-1}
 export QT_QPA_PLATFORM=${QT_QPA_PLATFORM:-xcb}
+export XAUTHORITY=${XAUTHORITY:-/tmp/.docker.xauth}
 export ARDUPILOT_HOME=${ARDUPILOT_HOME:-/opt/ardupilot}
 export FCU_URL=${FCU_URL:-"udp://:14551@"}
+export PULSE_SERVER=tcp:127.0.0.1:4800
 export PATH=/usr/lib/ccache:/opt/ardupilot/Tools/autotest:$PATH
 
 source /opt/ros/jazzy/setup.bash
